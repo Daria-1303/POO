02bd79c723910abe5ebf1bf6e4a5058a34a7cee4
Daria <daria.ciobanu@student.upt.ro>
2024-10-07 14:47:17 +0300

    ceas done ig

---
+++ Laboratoare/Lab_02/ClientCeas.java
@@ -0,0 +1,9 @@
+class ClientCeas{
+    public static void main(String args[]){
+        Ceas ceas1 = new Ceas();
+        ceas1.setOra(23);
+        ceas1.setMinut(20);
+        ceas1.setSecunda(30);
+        ceas1.afiseaza();
+    }
+}
