3b67dc4161e6167788a6d133807301fb7d19e970
Daria-1303 <daria.ciobanu@student.upt.ro>
2024-10-31 21:19:58 +0200

    upload some tasks

---
+++ testeLaborator/test1/alteCerinte/03/Client.java
@@ -0,0 +1,8 @@
+/*
+Creează un program care gestionează o bibliotecă de articole științifice. Fiecare articol are un titlu, autor și un an de publicare. Creează două clase, BibliotecaArticole și Articol, și implementează:
+
+adaugaArticol(Articol articol): adaugă un articol în bibliotecă doar dacă nu există deja un articol cu același titlu și autor.
+afiseazaArticole(): afișează toate articolele din bibliotecă în formatul "Titlu: [titlu], Autor: [autor], An: [an]".
+articoleDupaAn(int an): returnează o listă cu toate articolele publicate într-un anumit an.
+articoleAutor(String autor): returnează o listă cu toate articolele scrise de un anumit autor.
+ */
