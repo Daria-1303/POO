8591aacf662a5b6aa1478b3e7087cca303865e4c
Daria-1303 <daria.ciobanu@student.upt.ro>
2024-10-31 13:45:08 +0200

    update Desen - Figura

--- testeLaborator/test1/Mosteniri/Subiectul_1/Figura.java
+++ testeLaborator/test1/Mosteniri/Subiectul_1/Figura.java
@@ -2,7 +2,25 @@ class Figura{
     private double arie;
 
 
-    public Figra(int arie){
+    public Figura(int arie){
         this.arie = arie;
+    }
+
+    //getter
+
+    public double getArie(){
+        return arie;
+    }
+
+    public boolean equals(Object obj) {
+        if(obj instanceof Figura){
+            Figura otherFigura = (Figura) obj;
+            return this.getArie() == otherFigura.getArie();
+        }
+        return false;
+    }
+
+    public String toString(){
+        return "Figura - Arie : " + arie;
     }
 }
