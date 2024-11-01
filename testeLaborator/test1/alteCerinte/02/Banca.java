8a310262a43fa6798541875366aaa3d50923f2c3
Daria <daria.ciobanu@student.upt.ro>
2024-11-01 18:05:46 +0200

    Banca started

---
+++ testeLaborator/test1/alteCerinte/02/Banca.java
@@ -0,0 +1,25 @@
+class Banca{
+    private ContBancar conturi[];
+    private int numarConturi;
+
+    public Banca(){
+        this.conturi = new ContBancar[100];
+        this.numarConturi = 0;
+    }
+
+    public boolean adaugaCont(ContBancar contAdaugat){
+        if(numarConturi < 100){
+            for(int i = 0; i < numarConturi; i++){
+                if(conturi[i].equals(contAdaugat)){
+                    return false;
+                }
+
+                conturi[numarConturi ++] = contAdaugat;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
--- testeLaborator/test1/alteCerinte/02/Client.java
+++ testeLaborator/test1/alteCerinte/02/Client.java
@@ -1,8 +1,20 @@
 /*
-Implementează un program pentru o bancă care gestionează mai multe conturi bancare. Fiecare cont are un număr unic, un nume de titular și un sold. Creează două clase, Banca și ContBancar, și implementează:
+Implementează un program pentru o bancă care gestionează mai multe conturi bancare. 
+
+Fiecare cont are un număr unic, un nume de titular și un sold. 
+
+Creează două clase, Banca și ContBancar, și implementează:
 
 adaugaCont(ContBancar cont): adaugă un cont în bancă doar dacă nu există deja un cont cu același număr unic.
-afiseazaConturi(): afișează toate conturile în formatul "Număr Cont: [numar], Titular: [titular], Sold: [sold]".
+
+afiseazaConturi(): afișează toate conturile în formatul "Număr Cont: [numar], 
+
+Titular: [titular], Sold: [sold]".
+
+
 soldTotal(): calculează și returnează soldul total al tuturor conturilor din bancă.
-conturiSubSoldMinim(double soldMinim): returnează o listă cu toate conturile care au un sold mai mic decât valoarea specificată.
+
+conturiSubSoldMinim(double soldMinim): returnează o listă cu toate conturile care au un sold mai 
+
+mic decât valoarea specificată.
  */
