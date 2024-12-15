76685bd817a3693ff2b003b308c14c4406a41159
Daria-1303 <daria.ciobanu@student.upt.ro>
2024-12-15 17:14:16 +0200

    added bac with exception

--- testeLaborator/Test2/Test_04/Main.java
+++ testeLaborator/Test2/Test_04/Main.java
@@ -87,6 +87,40 @@ class Bac{
     }
 }
 
+class Bac2{
+    private double greutateMaxima;
+    private ArrayList<Autovehicul> autovehiculeDePeBac;
+    private double greutateCurenta; // sau putem calcula greutatea curenta la fiecare adaugare de autovehicul si implementarea metodei
+
+    public Bac2(double greutateMaxima){
+        this.greutateMaxima = greutateMaxima;
+        this.autovehiculeDePeBac = new ArrayList<Autovehicul>();
+        this.greutateCurenta = 0;
+    }
+
+    public boolean adaugaAutovehicul(Autovehicul autovehiculDeAdaugat) throws DepasesteGreutateMaxima{
+        if(greutateCurenta + autovehiculDeAdaugat.calculeazaGreutateTotala() > greutateMaxima){
+            throw new DepasesteGreutateMaxima("Greutatea totala depaseste greutatea maxima");
+            //return false;
+        }
+
+        greutateCurenta += autovehiculDeAdaugat.calculeazaGreutateTotala();
+        autovehiculeDePeBac.add(autovehiculDeAdaugat);
+
+        return true;
+    }
+
+    public String toString(){
+        return "Bac - Greutate maxima : " + greutateMaxima + "\n \t Autovehicule ->" + autovehiculeDePeBac.toString();
+    }
+}
+
+class DepasesteGreutateMaxima extends Exception{
+    public DepasesteGreutateMaxima(String mesaj){
+        super(mesaj);
+    }
+}
+
 class Main{
     public static void main(String[] args) {
         Autoturism a1 = new Autoturism("CS 13 SKY", 4, 0.3);
@@ -101,6 +135,30 @@ class Main{
         Bac b1 = new Bac(20);
         b1.adaugaAutovehicul(c1);
         b1.adaugaAutovehicul(a3); // nu l va afisa din cauza greutatii
+
+        System.out.println(b1.toString());
+    }
+}
+
+class Main2{
+    public static void main(String[] args) {
+        Autoturism a1 = new Autoturism("CS 13 SKY", 4, 0.3);
+        Autoturism a2 = new Autoturism("CS 26 SKY", 5, 0.25);
+        Autoturism a3 = new Autoturism("CS 04 SKY", 4.5, 0.15);
+
+        Camion c1 = new Camion("MH 78 FIL", 10);
+
+        c1.adaugaAutoturism(a1);
+        c1.adaugaAutoturism(a2);
+
+        Bac2 b1 = new Bac2(20);
+
+        try{
+            b1.adaugaAutovehicul(c1);
+            b1.adaugaAutovehicul(a3); // nu l va afisa din cauza greutatii
+        }catch(DepasesteGreutateMaxima e){
+            System.out.println(e.getMessage());
+        }
 
         System.out.println(b1.toString());
     }
